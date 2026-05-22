4d7335dc7b9ce1d4e4e64b1ee4cd5853eb7eb0bc
kunwarsandeep <kunwarsandeep987@gmail.com>
2026-05-22 21:46:00 +0545

    Add .gitignore and remove pycache tracking

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World! This is my first Flask website."
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

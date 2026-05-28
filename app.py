bf1b25f8910ad2ba67deea752fefe737c13f4163
Prajwal tamang <144401426+mrnobody-999@users.noreply.github.com>
2026-05-28 21:42:08 +0545

    Auto-commit: saving changes

--- app.py
+++ app.py
@@ -1,67 +1,7 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-# Home Page (Figma Homepage)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
+from Pet_Care.app import create_app
 
 
-# Register Page
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-
-    if request.method == "POST":
-
-        name = request.form["name"]
-        email = request.form["email"]
-        password = request.form["password"]
-
-        return f"""
-        Name: {name}<br>
-        Email: {email}<br>
-        Password: {password}
-        """
-
-    return render_template("register.html")
-
-
-# Login Page
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
-# Dashboard Page
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-
-# Pets Page
-
-@app.route("/pets")
-def pets():
-    return render_template("pet.html", title="Pets | Pet Care")
-
-
-# Profile Page
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-
-# Reset Password Page
-
-@app.route("/reset_password")
-def reset_password():
-    return render_template("reset_password.html")
+app = create_app()
 
 
 if __name__ == "__main__":

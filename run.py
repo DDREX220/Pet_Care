4bc5b901eaa777c1fa04174bddda603251648285
PrajwalTamangxd <prajaltamang73@gmail.com>
2026-06-19 18:03:46 +0545

    Fix run.py path for clean project structure

--- run.py
+++ run.py
@@ -1,15 +1,8 @@
 from pathlib import Path
 import sys
-
-
-project_dir = Path(__file__).resolve().parent / "Pet_Care"
+project_dir = Path(__file__).resolve().parent
 sys.path.insert(0, str(project_dir))
-
 from app import create_app
-
-
 app = create_app()
-
-
 if __name__ == "__main__":
     app.run(debug=True)

5e43a5a051ffa22f2531bca64184575cd3fc559c
kunwarsandeep <kunwarsandeep987@gmail.com>
2026-06-19 19:06:10 +0545

    fix: add MySQL port to database connection

--- app/database.py
+++ app/database.py
@@ -9,6 +9,7 @@ class Database:
             user=config.MYSQL_USER,
             password=config.MYSQL_PASSWORD,
             database=config.MYSQL_DATABASE,
+            port=config.MYSQL_PORT,
             cursorclass=pymysql.cursors.DictCursor,
         ) 
 
